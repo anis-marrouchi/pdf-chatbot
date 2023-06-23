5fc536ae9dbf0a2e1e8f1360c2311b428b3f0a79
Anis Marrouchi <anismarrouchi@hotmail.com>
2023-06-23 20:13:26 +0100

    merge balazsorban44 merge request into main

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import GitHub from 'next-auth/providers'
 
 declare module 'next-auth' {
   interface Session {
+    // @ts-ignore
     user: {
       /** The user's id. */
       id: string
@@ -14,20 +15,23 @@ export const {
   handlers: { GET, POST },
   auth,
   CSRF_experimental
+  // @ts-ignore
 } = NextAuth({
   providers: [GitHub],
   callbacks: {
+    // @ts-ignore
     jwt: async ({ token, profile }) => {
       if (profile) {
         token.id = profile.id
         token.image = profile.picture
       }
       return token
-    }
-    // @TODO
-    // authorized({ request, auth }) {
-    //   return !!auth?.user
-    // }
+    },
+    // @ts-ignore
+    authorized({ auth }) {
+      return !!auth?.user
+    },
+    trustHost: true
   },
   pages: {
     signIn: '/sign-in'
